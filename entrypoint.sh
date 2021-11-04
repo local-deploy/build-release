e39ff817b99bc7153b9064ae0c9d9d5d1c012c28
Sergey Voloshin <git@varme.pw>
2021-11-04 13:09:08 +0300

    fix tar

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,14 @@
 set -e
 
 PROJECT_ROOT="/go/src/github.com/${GITHUB_REPOSITORY}"
-ARTIFACTS_PATH="${GITHUB_WORKSPACE}/.artifacts"
+BINARY_PATH="${GITHUB_WORKSPACE}/bin"
 BINARY_NAME=$(basename "${GITHUB_REPOSITORY}")
 RELEASE_TAG=$(basename "${GITHUB_REF}")
 RELEASE_ASSET_NAME=${BINARY_NAME}-${RELEASE_TAG}
 TARGETS=("darwin/amd64" "linux/amd64")
 
 echo "----> Setting up repository"
-mkdir -p "${ARTIFACTS_PATH}"
+mkdir -p "${BINARY_PATH}"
 mkdir -p "${PROJECT_ROOT}"
 cp -a "${GITHUB_WORKSPACE}"/* "${PROJECT_ROOT}"/
 cd "${PROJECT_ROOT}"
@@ -21,7 +21,7 @@ go mod download
 for target in "${TARGETS[@]}"; do
   os="$(echo "${target}" | cut -d '/' -f1)"
   arch="$(echo "${target}" | cut -d '/' -f2)"
-  output="${ARTIFACTS_PATH}/${BINARY_NAME}_${os}_${arch}"
+  output="${BINARY_PATH}/${BINARY_NAME}_${os}_${arch}"
 
   echo "----> Building project for: ${target}"
   GOOS=${os} GOARCH=${arch} CGO_ENABLED=0 go build -o "${output}"
@@ -29,10 +29,9 @@ done
 
 echo "----> Prepare config files"
 cd "${GITHUB_WORKSPACE}"
-cp -r config-files "${ARTIFACTS_PATH}"
 
 echo "----> Compress files"
-tar cvfz "${RELEASE_ASSET_NAME}".tar.gz .artifacts/*
+tar cvfz "${RELEASE_ASSET_NAME}".tar.gz config-files bin
 
 echo "----> Build is complete"
 
